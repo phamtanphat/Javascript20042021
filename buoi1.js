e64a9d4acc1d0c3902ace08dbeaccb2363eebd55
phatdroid94@gmail.com <phatdroid94@gmail.com>
2021-04-22 18:28:11 +0700

    thay doi file buoi1

---
+++ buoi1.js
@@ -0,0 +1,13 @@
+// 1 : tạo thư mục quản lý source code .git
+// git init
+// 2 : kiểm tra sự thay đổi của folder
+// git status
+// 3 : lưu trữ sự thay đổi
+// git add tenfile (lưu sự thay đổi 1 file)
+// git add . (lưu tất cả sự thay đổi)
+// 4 : Commit vào repository
+// git commit -m "lưu file buoi1"
+
+// git config --global user.name "nguyen van A"
+// git config --global user.email "nguyenvana@gmail.com"
+
